b48a13b10661963bb4ec459e47190899a660d5e8
esp1 <espen.limi@gmail.com>
2021-10-21 23:07:03 +0200

    Upload and download work

--- src/main/java/bacit/web/bacit_models/FileModel.java
+++ src/main/java/bacit/web/bacit_models/FileModel.java
@@ -1,4 +1,38 @@
 package bacit.web.bacit_models;
 
 public class FileModel {
+
+    private String name;
+    private byte[] contents;
+    private String mimeType;
+
+    public FileModel(String name, byte[] contents, String mimeType) {
+        this.name = name;
+        this.contents = contents;
+        this.mimeType = mimeType;
+    }
+
+    public String getMimeType() {
+        return mimeType;
+    }
+
+    public void setMimeType(String mimeType) {
+        this.mimeType = mimeType;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public byte[] getContents() {
+        return contents;
+    }
+
+    public void setContents(byte[] contents) {
+        this.contents = contents;
+    }
 }
